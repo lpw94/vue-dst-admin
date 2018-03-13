ee1b9405aa51323502aefd4afd1f24d59600280e
lpw94 <lpw94@qq.com>
2018-03-13 13:43:18 +0800

    添加demo2

---
+++ src/views/systemSetup/Demo2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="animated fadeIn">
+
+    <Row>
+      <Col :sm="24" :md="24" :lg="12">
+        <div class="table-top-button">
+	      <Button>批量导入</Button>
+	      <Button>批量导出222222</Button>
+	    </div>
+
+      </Col>
+    </Row>
+    
+  </div>
+</template>
+
+   
+<script>
+</script>
